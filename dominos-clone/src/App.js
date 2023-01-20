a0e05051bc248911e3fbe1052dd4142b3a1dc817
sukanta255 <sukantap872@gmail.com>
2023-01-20 13:07:55 +0530

    Navbar completed

--- dominos-clone/src/App.js
+++ dominos-clone/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from "./Components/Navbar/navbar"
 
 function App() {
   return (
     <div className="App">
-      <h1>Dominos App</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ dominos-clone/src/Components/Loader/loading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./loading.css";
+
+const Loader = () => {
+  return (
+    <div className='loader'>Loading...</div>
+  )
+}
+
+export default Loader;
