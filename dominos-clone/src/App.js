8ae66be272fb3f6a841050ecb1d66f02350afc9a
sukanta255 <sukantap872@gmail.com>
2023-01-22 20:44:26 +0530

    Updated homepage

--- dominos-clone/src/App.js
+++ dominos-clone/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from "./Components/Navbar/navbar";
 import Footer from './Components/Footer/footer';
+import Homepage from "./Components/Homepage/homepage";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Homepage />
       <Footer />
     </div>
   );
